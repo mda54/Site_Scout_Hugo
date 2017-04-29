535f2816c79131f79bb8c89d9fd64597b7d4efaa
Alexandrecorre <alexandre.corre@isep.fr>
2017-04-29 21:00:11 +0200

    responsive + acceuil

---
+++ Accueil.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<?php include("Entete.php"); ?>
+</br>
+<p class="corpst">TEST:"L’association a pour but de former des citoyens libres, heureux, engagés et artisans de paix. Pour aider chaque jeune à grandir dans sa relation à soi, aux autres et à Dieu, elle mise sur une pédagogie du jeu, du projet, de l’équipe, de la vie dans la nature et de la promesse, et cherche à éduquer les enfants et les jeunes à la citoyenneté, à la solidarité, au respect de l’environnement, au cheminement spirituel et à l’ouverture aux autres.</br>
+
+Concrètement, les Scouts et Guides de France proposent aux jeunes filles et garçons de 6 à 20 ans un espace de vie et des activités qui répondent à leur besoin de rêver, d’agir, de réussir leurs projets, de vivre en communauté, de donner sens à leur vie. Les équipes sont soutenues dans leurs projets et encadrées par des jeunes chefs et cheftaines bénévoles, Les Scouts et Guides de France participent à de nombreuses activités d’intérêt général dans les domaines de la solidarité, de l’environnement et de la sécurité civile. Ils misent sur la co‑éducation des garçons et des filles et accueillent des jeunes, de milieux sociaux, de cultures ou de religions différentes."
+</p>
+
+
+</body>
+
+</html>
