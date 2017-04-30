22bcf99332f44da8b0c23b23b7969eb0045b45c1
Alexandrecorre <alexandre.corre@isep.fr>
2017-04-30 13:37:42 +0200

    Ajout css des Vues

--- Vue/Calendrier.php
+++ Vue/Calendrier.php
@@ -1,5 +1,8 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html>
+<head>
+  <link rel="stylesheet" href="../Vue/Calendrier.css" type="text/css" />
+</head>
 <?php include("Entete.php"); ?>
 </br>
 
