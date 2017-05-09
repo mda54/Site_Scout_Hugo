b9492091b46413e8e50e03e6e1688d0c764d96df
Alexandrecorre <alexandre.corre@isep.fr>
2017-05-09 22:41:14 +0200

    Maj nouvelle entete

---
+++ Old/Entete.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+   <title>Accueil-Scouts Saint-Fiacre Pulnoy</title>
+   <meta http-equiv="Content-Language" content="fr" />
+   <link rel="stylesheet" href="../Vue/Entete.css" type="text/css" />
+   <script type="text/javascript" src="../Modele/function.js"></script>
+   <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
+</head>
+
+<body class="entete">
+  <header>
+    <a href="Accueil.php"><img class="imgtete" src="../Images/bandeau.jpg" alt="doisneau" /></a>
+  </header>
+<!-- Debut code HTML du menu -->
+<div class="Menu">
+<!-- Debut Menu 1 -->
+<div class="menu" id="Menu1" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu1Titre">Renseignements </div>
+
+  <div class="contenuMenu" id="Menu1Item" style="display: none">
+      <div class="itemOut1"><a href="../Controleur/Renseignement_Scoutisme.php" class="lienMenu">Le scoutisme</a></div>
+      <div class="itemOut1"><a href="../Controleur/Contacts.php?id=visiteur" class="lienMenu">Contacts</a></div>
+      <div class="itemOut1"><a href="../Controleur/Calendrier.php?id=visiteur" class="lienMenu">Calendrier des réunions</a></div>
+      <div class="itemOut1"><a href="../Controleur/Renseignement_Partenaires.php" class="lienMenu">Les partenaires</a></div>
+      <div class="itemOut1"><a href="../Controleur/Renseignement_Questionnaire.php" class="lienMenu">Questionnaire</a></div>
+
+
+  </div>
+
+</div>
+<!-- Fin Menu 1 -->
+<!-- Debut Menu 2 -->
+<div class="menu" id="Menu2" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu2Titre">Farfadets</div>
+
+  <div class="contenuMenu" id="Menu2Item" style="display: none">
+      <div class="itemOut2"><a href="../Controleur/Contacts.php?id=Farfadets" class="lienMenu">Contacts</a></div>
+      <div class="itemOut2"><a href="../Controleur/Calendrier.php?id=Farfadets" class="lienMenu">Calendrier</a></div>
+      <div class="itemOut2"><a href="../Controleur/Photos.php?id=Farfadets" class="lienMenu">Photos</a></div>
+      <div class="itemOut2"><a href="../Controleur/Articles.php?id=Farfadets" class="lienMenu">Articles</a></div>
+      <div class="itemOut2"><a href="../Controleur/Forum.php?id=Farfadets" class="lienMenu">Forum</a></div>
+  </div>
+</div>
+<!-- Fin Menu 2 -->
+<!-- Debut Menu 3 -->
+<div class="menu" id="Menu3" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu3Titre">Louveteaux/Jeanettes</div>
+
+  <div class="contenuMenu" id="Menu3Item" style="display: none">
+
+      <div class="itemOut3"><a href="../Controleur/Contacts.php?id=Louveteaux" class="lienMenu">Contacts</a></div>
+      <div class="itemOut3"><a href="../Controleur/Calendrier.php?id=Louveteaux" class="lienMenu">Calendrier</a></div>
+      <div class="itemOut3"><a href="../Controleur/Photos.php?id=Louveteaux" class="lienMenu">Photos</a></div>
+      <div class="itemOut3"><a href="../Controleur/Articles.php?id=Louveteaux" class="lienMenu">Articles</a></div>
+      <div class="itemOut3"><a href="../Controleur/Forum.php?id=Louveteaux" class="lienMenu">Forum</a></div>
+  </div>
+
+</div>
+<!-- Fin Menu 3 -->
+<!-- Debut Menu 4 -->
+<div class="menu" id="Menu4" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu4Titre">Scouts</div>
+
+  <div class="contenuMenu" id="Menu4Item" style="display: none">
+
+      <div class="itemOut4"><a href="../Controleur/Contacts.php?id=Scouts" class="lienMenu">Contacts</a></div>
+      <div class="itemOut4"><a href="../Controleur/Calendrier.php?id=Scouts" class="lienMenu">Calendrier</a></div>
+      <div class="itemOut4"><a href="../Controleur/Photos.php?id=Scouts" class="lienMenu">Photos</a></div>
+      <div class="itemOut4"><a href="../Controleur/Articles.php?id=Scouts" class="lienMenu">Articles</a></div>
+      <div class="itemOut4"><a href="../Controleur/Forum.php?id=Scouts" class="lienMenu">Forum</a></div>
+  </div>
+
+</div>
+<!-- Fin Menu 4 -->
+<!-- Debut Menu 5 -->
+<div class="menu" id="Menu5" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu5Titre">Pionniers/Caravelles</div>
+
+  <div class="contenuMenu" id="Menu5Item" style="display: none">
+
+      <div class="itemOut5"><a href="../Controleur/Contacts.php?id=Pionniers" class="lienMenu">Contacts</a></div>
+      <div class="itemOut5"><a href="../Controleur/Calendrier.php?id=Pionniers" class="lienMenu">Calendrier</a></div>
+      <div class="itemOut5"><a href="../Controleur/Photos.php?id=Pionniers" class="lienMenu">Photos</a></div>
+      <div class="itemOut5"><a href="../Controleur/Articles.php?id=Pionniers" class="lienMenu">Articles</a></div>
+      <div class="itemOut5"><a href="../Controleur/Forum.php?id=Pionniers" class="lienMenu">Forum</a></div>
+  </div>
+
+</div>
+<!-- Fin Menu 5 -->
+<!-- Debut Menu 6 -->
+<div class="menu" id="Menu6" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu6Titre">Compagnons</div>
+
+  <div class="contenuMenu" id="Menu6Item" style="display: none">
+
+      <div class="itemOut6"><a href="../Controleur/Contacts.php?id=Pionniers" class="lienMenu">Contacts</a></div>
+      <div class="itemOut6"><a href="../Controleur/Calendrier.php?id=Pionniers" class="lienMenu">Calendrier</a></div>
+      <div class="itemOut6"><a href="../Controleur/Photos.php?id=Pionniers" class="lienMenu">Photos</a></div>
+      <div class="itemOut6"><a href="../Controleur/Articles.php?id=Pionniers" class="lienMenu">Articles</a></div>
+      <div class="itemOut6"><a href="../Controleur/Forum.php?id=Pionniers" class="lienMenu">Forum</a></div>
+  </div>
+
+</div>
+<!-- Fin Menu 6 -->
+<!-- Debut Menu 7 -->
+<div class="menu" id="Menu7" onmouseout="fout(this)" onmouseover="fover(this)">
+  <div class="titreMenu itemOut" id="Menu7Titre">Chefs Cheftaines</div>
+
+  <div class="contenuMenu" id="Menu7Item" style="display: none">
+
+      <div class="itemOut7"><a href="../Controleur/Contacts.php?id=Chefs" class="lienMenu">Contacts</a></div>
+      <div class="itemOut7"><a href="../Controleur/Calendrier.php?id=Chefs" class="lienMenu">Calendrier</a></div>
+      <div class="itemOut7"><a href="../Controleur/Photos.php?id=Chefs" class="lienMenu">Photos</a></div>
+      <div class="itemOut7"><a href="../Controleur/Articles.php?id=Chefs" class="lienMenu">Articles</a></div>
+      <div class="itemOut7"><a href="../Controleur/Forum.php?id=Chefs" class="lienMenu">Forum</a></div>
+  </div>
+
+</div>
+</div>
+</body>
+
+</html>
