0796d7ef248804f0b88980ac4c758ca1a5613f03
AlexandreCorre <alexandre.corre@isep.fr>
2017-05-14 23:22:12 +0200

    avancement maj entete

--- entete.php
+++ entete.php
@@ -1,26 +1,48 @@
 <?php
 include_once'fonction/langue.php';
+session_start();
+if (!isset($_SESSION['langue']) and isset($_GET['langue'])) {
+  $_SESSION['langue']=$_GET['langue'];
+}
 ?>
 <html>
 <head>
    <title>Accueil-Scouts Saint-Fiacre Pulnoy</title>
    <meta http-equiv="Content-Language" content="fr" />
    <link rel="stylesheet" href="css/entete.css" type="text/css" />
-   <script type="text/javascript" src="../js/function.js"></script>
+   <script type="text/javascript" src="js/function.js"></script>
    <meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 </head>
 <body class="entete" background="images/Fond.jpg">
   <div class="menu">
-    <ul>
-      <li><?php echo Renseignements?></li>
-      <li><?php echo Farfadets ?></li>
-      <li><?php echo 'Louveteaux  Jeanettes' ?></li>
-      <li><?php echo Scouts ?></li>
-      <li><?php echo 'Pionniers  Caravelles' ?></li>
-      <li><?php echo Compagnons ?></li>
-      <li><?php echo Chefs ?></li>
+    <ul class="liste">
+      <li class="liste"><?php echo Renseignements?></li>
+      <li class="liste"><?php echo Farfadets ?></li>
+      <li class="liste"><?php echo 'Louveteaux  Jeanettes' ?></li>
+      <li class="liste"><?php echo Scouts ?></li>
+      <li class="liste"><?php echo 'Pionniers  Caravelles' ?></li>
+      <li class="liste"><?php echo Compagnons ?></li>
+      <li class="liste"><?php echo Chefs ?></li>
     </ul>
   </div>
+  <ul class="langue">
+        <?php
+      if (isset($_SESSION['langue'])) {
+        ?>
+        <p><?php echo strtoupper($_SESSION['langue']) ?><span id="none"></span></p>
+        <?php
+      }
+      else {
+        ?>
+        <p>FR<span id="none"></span></p>
+        <?php
+      }
+    ?>
+        <ul class="choix">
+            <li id="bottom1"><a href="<?php echo $url ?>?langue=fr">FR</a></li>
+            <li id="bottom2"><a href="<?php echo $url ?>?langue=en">EN</a></li>
+        </ul>
+  </ul>
 </body>
 
 </html>
