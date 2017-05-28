ddc14ac39357591a1b9b8fd0b21a7fee7e95233f
AlexandreCorre <alexandre.corre@isep.fr>
2017-05-28 22:56:13 +0200

    finalisation

--- entete.php
+++ entete.php
@@ -38,38 +38,30 @@ if (isset($_GET['key'])){
         <img src="images/entete.svg" alt="logo"/>
           <ul class="liste">
             <li class="menuac liste" ><a class="menuac liste"  href="index.php">
-
               <?php echo Accueil?>
             </a></li>
 
             <li class="menuren liste" ><a class="menuren liste"  href="renseignement.php">
-
                <?php echo Renseignements?>
              </a></li>
 
             <li class="menufar liste" ><a class="menufar liste"  href="groupes.php?key=farfadets"> 
-
-               <?php echo Farfadets ?> 
+               <?php echo Farfadets ?>
              </a></li>
 
-            <li class="menulou liste" ><a class="menulou liste"  href="groupes.php?key=louveteaux"> 
-
+            <li class="menulou liste" ><a class="menulou liste"  href="groupes.php?key=louveteaux">
               <?php echo Louveteaux ?>
             </a></li>
 
-            <li class="menusco liste" ><a class="menusco liste"  href="groupes.php?key=scouts"> 
-
+            <li class="menusco liste" ><a class="menusco liste"  href="groupes.php?key=scouts">
                <?php echo Scouts ?>
              </a></li>
 
-            <li class="menupio liste" ><a class="menupio liste"  href="groupes.php?key=pionniers"> 
-
+            <li class="menupio liste" ><a class="menupio liste"  href="groupes.php?key=pionniers">
              <?php echo Pionniers ?>
            </a></li>
 
-
-            <li class="menucom liste"  ><a class="menucom liste"  href="groupes.php?key=compagnons"> 
-
+            <li class="menucom liste"  ><a class="menucom liste"  href="groupes.php?key=compagnons">
              <?php echo Compagnons ?>
            </a></li>
           </ul>
@@ -98,7 +90,7 @@ if (isset($_GET['key'])){
         </ul>
 
         <div class="logo">
-           <?php if ($index=="all") {?> 
+           <?php if ($index=="all") {?>
            <!--logos renseignement accueil-->
 
             <a href="articles.php?key=all"><img src="images/logo_accueil/article.svg" alt="article"/></a>
@@ -106,7 +98,7 @@ if (isset($_GET['key'])){
             <a href="contact.php?key=all"><img src="images/logo_accueil/contact.svg" alt="contact"/></a>
             <a href="photos.php?key=all"><img src="images/logo_accueil/photo.svg" alt="photo"/></a>
             <?php
-          }else { ?> logos autres pages rappel fct du haut
+          }else { ?> <!--logos autres pages rappel fct du haut-->
 
             <a href='articles.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/article.svg' alt="article"/></a>
             <a href='calendrier.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/calendrier.svg' alt="calendrier"/></a>
--- groupes.php
+++ groupes.php
@@ -59,7 +59,7 @@ if ($index=="all") {
   <!--calendrier-->
   <div class="iframe">
   	<fieldset class="ssiframe">
-  		<legend>Calendrier</legend>
+  		<legend><?php echo Calendrier ?></legend>
   			<iframe src="https://calendar.google.com/calendar/embed?showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23ffffff&amp;src=saintfiacre.pulnoy%40gmail.com&amp;color=%23333333&amp;src=o87a1o1eqjiv167tm8g152po9s%40group.calendar.google.com&amp;color=%23853104&amp;src=vjo6gs4nmah3hh1im7cncv471c%40group.calendar.google.com&amp;color=%23711616&amp;src=bo0mhpvkpv9v3lt8uv42us0d48%40group.calendar.google.com&amp;color=%23182C57&amp;src=e_2_fr%23weeknum%40group.v.calendar.google.com&amp;color=%238C500B&amp;ctz=Europe%2FParis" style="border-width:0" width="700" height="500" frameborder="0" scrolling="no"></iframe>
   	</fieldset>
   </div>
