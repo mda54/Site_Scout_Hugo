573425516f6cefb8f6672ead106c0b399c23c739
mda54 <manond@live.fr>
2017-05-28 21:33:14 +0200

    entete

--- entete.php
+++ entete.php
@@ -1,5 +1,11 @@
-
 <!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Accueil-Scouts Saint-Fiacre Pulnoy</title>
+    <link rel="stylesheet" href="css/entete.css" type="text/css"/>
+    <script type="text/javascript" src="js/function.js"></script>
+  </head>
 <?php
 if (isset($_GET['deco']) and $_GET['deco']==true) {
   session_start();
@@ -26,32 +32,53 @@ if (isset($_GET['key'])){
 }else {  $index="all";}
 
 ?>
-<html>
-    <head>
-       <title>Accueil-Scouts Saint-Fiacre Pulnoy</title>
-       <meta http-equiv="Content-Language" content="fr">
-       <link rel="stylesheet" href="css/entete.css" type="text/css"/>
-       <script type="text/javascript" src="js/function.js"></script>
-       <meta http-equiv="Content-Type" content="text/html" charset="utf-8">
-    </head>
 
-    <body class="entete" background="images/Fond.jpg">
+   <body class="entete" background="images/Fond.jpg">
       <div class="menu">
         <img src="images/entete.svg" alt="logo"/>
           <ul class="liste">
-            <li class="menuac" class="liste" ><a class="menuac" class="liste"  href="index.php"> <?php echo Accueil?></a></li>
-            <li class="menuren" class="liste" ><a class="menuren" class="liste"  href="renseignement.php"> <?php echo Renseignements?></a></li>
-            <li class="menufar" class="liste" ><a class="menufar" class="liste"  href="groupes.php?key=farfadets"> <?php echo Farfadets ?></a></li>
-            <li class="menulou" class="liste" ><a class="menulou" class="liste"  href="groupes.php?key=louveteaux"> <?php echo Louveteaux ?></a></li>
-            <li class="menusco" class="liste" ><a class="menusco" class="liste"  href="groupes.php?key=scouts"> <?php echo Scouts ?></a></li>
-            <li class="menupio" class="liste" ><a class="menupio" class="liste"  href="groupes.php?key=pionniers"> <?php echo Pionniers ?></a></li>
-            <li class="menucom" class="liste"  ><a class="menucom" class="liste"  href="groupes.php?key=compagnons"> <?php echo Compagnons ?></a></li>
+            <li class="menuac liste" ><a class="menuac liste"  href="index.php">
+
+              <?php echo Accueil?>
+            </a></li>
+
+            <li class="menuren liste" ><a class="menuren liste"  href="renseignement.php">
+
+               <?php echo Renseignements?>
+             </a></li>
+
+            <li class="menufar liste" ><a class="menufar liste"  href="groupes.php?key=farfadets"> 
+
+               <?php echo Farfadets ?> 
+             </a></li>
+
+            <li class="menulou liste" ><a class="menulou liste"  href="groupes.php?key=louveteaux"> 
+
+              <?php echo Louveteaux ?>
+            </a></li>
+
+            <li class="menusco liste" ><a class="menusco liste"  href="groupes.php?key=scouts"> 
+
+               <?php echo Scouts ?>
+             </a></li>
+
+            <li class="menupio liste" ><a class="menupio liste"  href="groupes.php?key=pionniers"> 
+
+             <?php echo Pionniers ?>
+           </a></li>
+
+
+            <li class="menucom liste"  ><a class="menucom liste"  href="groupes.php?key=compagnons"> 
+
+             <?php echo Compagnons ?>
+           </a></li>
           </ul>
       </div>
       <div class="page">
         <!--balise choix langue-->
+
         <ul class="langue">
-              <?php
+               <?php
             if (isset($_SESSION['langue'])) {
               ?>
               <p><?php echo strtoupper($_SESSION['langue']) ?><span id="none"></span></p>
@@ -63,26 +90,30 @@ if (isset($_GET['key'])){
               <?php
             }
           ?>
-              <ul class="choix">
-                  <li id="bottom1"><a href="<?php echo $url ?>?langue=fr">FR</a></li>
-                  <li id="bottom2"><a href="<?php echo $url ?>?langue=en">EN</a></li>
-              </ul>
+
+        <ul class="choix">
+            <li id="bottom1"><a href="<?php echo $url ?>?langue=fr">FR</a></li>
+            <li id="bottom2"><a href="<?php echo $url ?>?langue=en">EN</a></li>
+        </ul>
         </ul>
 
         <div class="logo">
-          <?php if ($index=="all") {?> <!--logos renseignement accueil-->
+           <?php if ($index=="all") {?> 
+           <!--logos renseignement accueil-->
+
             <a href="articles.php?key=all"><img src="images/logo_accueil/article.svg" alt="article"/></a>
             <a href="calendrier.php?key=all"><img src="images/logo_accueil/calendrier.svg" alt="calendrier"/></a>
             <a href="contact.php?key=all"><img src="images/logo_accueil/contact.svg" alt="contact"/></a>
             <a href="photos.php?key=all"><img src="images/logo_accueil/photo.svg" alt="photo"/></a>
             <?php
-          }else { ?> <!--logos autres pages rappel fct du haut-->
-            <a href='articles.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/article.svg'alt="article"/></a>
+          }else { ?> logos autres pages rappel fct du haut
+
+            <a href='articles.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/article.svg' alt="article"/></a>
             <a href='calendrier.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/calendrier.svg' alt="calendrier"/></a>
             <a href='contact.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/contact.svg' alt="contact"/></a>
             <a href='photos.php?key=<?php echo $index ?>'><img src='images/<?php echo $index; ?>/photo.svg' alt="photo"/></a>
             <?php } ?>
-          </br>
+          <br>
         </div>
     </div>
     </body>
